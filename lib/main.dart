f65284e3d0759a31fcf7d1d2a234fead12e1a235
carlaval655 <carla.val655@gmail.com>
2022-08-21 22:02:04 -0400

    onPressed RaisedButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, deprecated_member_use, sort_child_properties_last, use_key_in_widget_constructors, avoid_print
+
 import 'package:flutter/material.dart';
 
 // void main(){
@@ -6,16 +8,36 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion(){
+    print('Answer chosen');
+  }
   @override
   //Devuelve un widget
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold( //Define el widget general de una app
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: Text('This is my default text! 1'),
-      ),
+      home: Scaffold(
+          //Define el widget general de una app
+          appBar: AppBar(
+            title: Text('My First App'),
+          ),
+          body: Column(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Text("The question!"),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: answerQuestion,
+              ),
+            ],
+          )),
     );
   }
 }
